c7318b353afe9babacef8af60dcfb84ccd147115
Mihir Thakkar <mihir9293@gmail.com>
2016-10-07 00:37:37 -0700

    Update account service class

--- src/csebank_dbmodel/asu/edu/AccountService.java
+++ src/csebank_dbmodel/asu/edu/AccountService.java
@@ -5,8 +5,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 
-
-
 public class AccountService {
 
 	ConnectionClass connectionClass=null;
@@ -48,16 +46,66 @@ public class AccountService {
 			return "";
 	}	
 	
+	public boolean deleteAccount()
+	{
+		boolean result=false;
+		String updateQuery="UPDATE Account A SET A.AccStatus='closed' WHERE A.accountId=?";
+		parameterNameValueMap.put("AccountId", accountId);
+		if(connectionClass.executeUpdateWithSQLQuery(updateQuery, parameterNameValueMap))
+			result=true;
+		return result;
+	}
+	
+	public boolean blockAccount()
+	{
+		boolean result=false;
+		String updateQuery="UPDATE Account A SET A.AccStatus='blocked' WHERE A.accountId=?";
+		parameterNameValueMap.put("AccountId", accountId);
+		if(connectionClass.executeUpdateWithSQLQuery(updateQuery, parameterNameValueMap))
+			result=true;
+		return result;
+	}
+	
 	public int getAccountBalance() 
 	{
-		String getBalanceQuery="SELECT AccBalance FROM Account WHERE AccountId=?";
+		String getBalanceQuery="SELECT AccBalance FROM Account WHERE accountId=?";
 		parameterNameValueMap.put("AccountId", parameterMap.get("AccountId"));
 		List<HashMap<String,Object>> resultList = connectionClass.executeSelectQuery(getBalanceQuery, parameterNameValueMap);
 		int balance=((Integer)resultList.get(0).get("AccBalance"));
 		return balance;
 	}
 	
-	public boolean updateAccount()
+	public Account getAccountDetailsOnAccId()
+	{
+		String selectQuery="SELECT * FROM Account where accountId=?";
+		Account account=null;
+		parameterNameValueMap.put("accountId", parameterMap.get("accountId"));
+		List<HashMap<String,Object>> resultlist=connectionClass.executeSelectQuery(selectQuery, parameterNameValueMap);
+		account=this.createAccountObject(resultlist).get(0);
+			return account;
+	}	
+
+	public List<Account> getAccountListOnUserId()
+	{
+		String selectQuery="SELECT * FROM Account where userId=?";
+		parameterNameValueMap.put("userId", parameterMap.get("userId"));
+		List<HashMap<String,Object>> resultlist=connectionClass.executeSelectQuery(selectQuery, parameterNameValueMap);
+		List<Account> accountList=this.createAccountObject(resultlist);
+			return accountList;
+	}
+	
+	public String checkAccountStatus()
+	{
+		String selectQuery="SELECT accountStatus FROM Account where accountId=?";
+		Account account=null;
+		parameterNameValueMap.put("accountId", parameterMap.get("accountId"));
+		List<HashMap<String,Object>> resultlist=connectionClass.executeSelectQuery(selectQuery, parameterNameValueMap);
+		account=this.createAccountObject(resultlist).get(0);
+		String status=resultList.get(0).get("accountStatus");
+			return status;
+	}	
+			
+	public boolean updateAccountBalance()
 	{
 		boolean result=false;
 		String transStatus = parameterMap.get("TransStatus");
@@ -86,4 +134,5 @@ public class AccountService {
 		}
 		return result;
 	}
+
 }
